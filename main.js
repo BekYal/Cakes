df70ed99be50cf44de9c20e0194535e6d6308dd4
DEABRO-BEK <hygohy2@gmail.com>
2021-01-08 14:48:17 +0300

    ВСЁ С НУЛЯ, но потом

--- main.js
+++ main.js
@@ -41,15 +41,74 @@ TileEntity.registerPrototype(BlockID.dough, {
 tick(){
   
   
-if(
-  
-  
-  World.getBlockID(this.x, this.y + 1, this.z) == 45 && World.getBlockID(this.x, this.y - 1, this.z) == 45 && World.getBlockID(this.x + 1, this.y, this.z + 1) == 45 && World.getBlockID(this.x - 1, this.y, this.z - 1) == 45 && World.getBlockID(this.x, this.y + 1, this.z + 1) == 45 && World.getBlockID(this.x + 1, this.y + 1, this.z) == 45 && World.getBlockID(this.x - 1, this.y + 1, this.z - 1) && World.getBlockID(this.x, this.y - 2, this.z) == 51){
-  if(World.getThreadTime()%60 == 0){
-World.setBlock(this.x, this.y, this.z, 80, 0)}
-    }
+
+let Pat = [
+[0, 1, 0],
+[0, -1, 0],
+[1, 0, 1],
+[-1, 0, -1],
+[0, 1, 1],
+[1, 1, 0],
+[-1 , 1, -1],
+]
+if(World.getThreadTime()%60==0){
+for(let i in Pat){
+let coord = Pat[i];
+if(this.blockSource.getBlockID(this.x + coord[0], this.y + coord[1], this.z + coord[2]) !== 45){return};
 }
-}); 
+if(this.blockSource.getBlockID(this.x, this.y - 2, this.z) == 51){return};
+this.blockSource.setBlock(this.z, this.y, this.z, 80, 0);
+}.
+  
+}});
+/*
+000
+000
+000
+
+000
+000
+000
+
+0х0
+000
+000
+
+World.getBlockID(this.x + 1, this.y, this.z + 1) == 45*/
+
+var render = new ICRender.Model();
+var model = BlockRenderer.createModel();
+ model.addBox(8/16, 8/16, 8/16, 8/16, 8/16, 8/16, [["ttt", 0]]); //undefined
+render.addEntry(model);
+BlockRenderer.setStaticICRender(BlockID.dough, 0, render);
+
+
+
+/*
+World.getBlockID(this.x, this.y + 1, this.z) == 45
+World.getBlockID(this.x, this.y - 1, this.z) == 45
+World.getBlockID(this.x, this.y + 1, this.z + 1) == 45
+World.getBlockID(this.x, this.y + 1, this.z - 1) == 45
+World.getBlockID(this.x + 1, this.y + 1, this.z + 1) == 45
+World.getBlockID(this.x - 1, this.y + 1, this.z + 1) == 45
+World.getBlockID(this.x + 1, this.y + 1, this.z) == 45
+World.getBlockID(this.x - 1, this.y + 1, this.z) == 45
+World.getBlockID(this.x - 1, this.y + 1, this.z - 1) == 45
+World.getBlockID(this.x + 1, this.y + 1, this.z - 1) == 45
+/*
+
+111
+111
+111
+
+000
+000
+000
+
+000
+010
+000
+*/
 
 
 
